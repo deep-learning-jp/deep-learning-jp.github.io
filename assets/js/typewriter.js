6d27ac93328553e2f8982c261a0dc8fbf42de9f5
Rishabh Gupta <rishabhgupta@RishabhnoMacBook-puro.local>
2018-09-13 23:14:21 +0900

    added support for google maps and dynamic name

---
+++ assets/js/typewriter.js
@@ -0,0 +1,1 @@
+function typeWriter(e,t,n,r){var i="";var s=t.length;var o=0;var u=new Audio(n);$(e).text("");var a=setInterval(function(){if(!u.paused){u.pause();u.currentTime=0}var n=i+t.charAt(o);$(e).text(n);if(t.charAt(o)!==" "){u.play()}i=n;if(o===s-1){clearInterval(a)}o=o+1},r)}
