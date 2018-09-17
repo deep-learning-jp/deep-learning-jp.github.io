d6cbaa30bb46295cc69d0602b9a26c07fcfbbafd
Rishabh Gupta <rishabhgupta@RishabhnoMacBook-puro.local>
2018-09-17 18:08:26 +0900

    minor changes

--- assets/js/typewriter_2.js
+++ assets/js/typewriter_2.js
@@ -2,20 +2,21 @@
 var app = document.getElementById('app_typewriter');
 
 var typewriter = new Typewriter(app, {
-    loop: true
+    loop: true,
+    cursor: ""
 });
 
 
 
 
-typewriter.typeString( '\xa0\xa0' +'Alex Lamb')
+typewriter.typeString( '\xa0\xa0' +'Alex Lamb'+ '\xa0')
     // .cursor('')
     .pauseFor(2500)
     .deleteAll()
-    .typeString('\xa0\xa0' + 'Researcher')
+    .typeString('\xa0\xa0' + 'Eminent Deep Learning Researcher' + '\xa0')
     .pauseFor(2500)
     .deleteAll()
-    .typeString ('\xa0\xa0' +'University of Montreal')
+    .typeString ('\xa0\xa0' +'University of Montreal'+ '\xa0')
     // .pauseFor(2500)
     // .deleteChars(7)
     // .typeString('<strong>altered!</strong>')
