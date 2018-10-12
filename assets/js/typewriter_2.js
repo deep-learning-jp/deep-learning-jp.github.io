3b624d90bf1c8e113d183cc4fcdd06d895b31d32
Rishabh Gupta <rishabhgupta@RishabhnoMacBook-puro.local>
2018-10-12 12:58:51 +0900

    added a break in the title

--- assets/js/typewriter_2.js
+++ assets/js/typewriter_2.js
@@ -11,14 +11,14 @@ var typewriter = new Typewriter(app, {
 
 typewriter.typeString( '\xa0\xa0' +'Kaz Sato'+ '\xa0')
     // .cursor('')
-    .pauseFor(2500)
+    .pauseFor(1500)
     .deleteAll()
     .typeString('\xa0\xa0' + 'Staff Developer Advocate' + '\xa0')
-    .pauseFor(2500)
+    .pauseFor(1500)
     .deleteAll()
     .typeString ('\xa0\xa0' +'Google Cloud, Google Inc'+ '\xa0')
     // .pauseFor(2500)
     // .deleteChars(7)
     // .typeString('<strong>altered!</strong>')
-    .pauseFor(2500)
+    .pauseFor(1500)
     .start();
